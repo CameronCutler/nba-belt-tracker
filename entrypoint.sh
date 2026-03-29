43980889678c0db512ce3b67fbfe6fded17b1966
Cameron Cutler <cameron.t.cutler@gmail.com>
2026-03-29 09:38:05 -0500

    main : railway log tweaks

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 set -e
 
-echo "Running database setup..."
-php /var/www/database/setup.php
+echo "Running migrations..."
+php /var/www/database/migrate.php
+
+echo "Seeding teams..."
+php /var/www/database/seed_teams_simple.php
+
+echo "Initialising belt..."
+php /var/www/database/init_belt.php
 
 echo "Starting services..."
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
